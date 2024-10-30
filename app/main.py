34f72487a983655b98aee615d4aa02d2d352c577
wjh <wjh2012@naver.com>
2024-10-30 14:19:41 +0900

    fix: 폴더 자동 생성 및 날짜 추가

--- app/main.py
+++ app/main.py
@@ -4,19 +4,21 @@ from datetime import datetime
 
 app = FastAPI()
 
-# 파일을 저장할 디렉토리 경로
-SAVE_DIRECTORY = Path("./uploads")
-SAVE_DIRECTORY.mkdir(parents=True, exist_ok=True)  # 디렉토리가 없으면 생성
-
+BASE_DIRECTORY = Path("./uploads")  # 기본 저장 경로
 
 @app.post("/upload/")
 async def upload_file(
-    request: Request,  # 클라이언트 IP 주소를 위해 추가
+    request: Request,
     file: UploadFile = File(...),
     data: str = Form(...)
 ):
+    # 현재 날짜를 기준으로 디렉토리 생성
+    current_date = datetime.now().strftime("%Y%m%d")
+    save_directory = BASE_DIRECTORY / current_date
+    save_directory.mkdir(parents=True, exist_ok=True)  # 디렉토리가 없으면 생성
+
     # 현재 시간을 밀리초 단위로 포맷팅하여 접두사로 추가
-    current_time = datetime.now().strftime("%Y%m%d_%H%M%S_%f")[:-3]  # 밀리초까지 포함하고 마지막 세 자리 제거
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S_%f")[:-3]
     client_ip = request.client.host  # 클라이언트 IP 주소 가져오기
 
     # 이미지와 텍스트 파일 이름에 시간과 IP 주소를 접두사로 추가
@@ -24,12 +26,12 @@ async def upload_file(
     text_filename = f"{current_time}_{client_ip}_uploaded_text.txt"
 
     # 이미지 파일 저장 경로 설정
-    image_path = SAVE_DIRECTORY / image_filename
+    image_path = save_directory / image_filename
     with image_path.open("wb") as buffer:
         buffer.write(await file.read())
 
     # 텍스트 파일 저장 경로 설정
-    text_path = SAVE_DIRECTORY / text_filename
+    text_path = save_directory / text_filename
     with text_path.open("w") as text_file:
         text_file.write(data)
 
